f54c70fe4cb5faecb0115886c2c7cccc455b545b
radj307 <radj307@gmail.com>
2022-06-20 15:36:45 -0400

    add an alternative recipe for earth charges
    This is to ease the process of creating sulfur dust in order to feed a mekanism fission reactor

---
+++ kubejs/server_scripts/atm7-plus/earth_charge.js
@@ -0,0 +1,7 @@
+onEvent('recipes', e => {
+  e.shaped('2x thermal:earth_charge',['GP ','PC ','   '],{
+    G: 'minecraft:gunpowder',
+    P: 'minecraft:blaze_powder',
+    C: '#minecraft:coals'
+  })
+})
